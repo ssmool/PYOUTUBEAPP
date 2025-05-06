cd59bf03cdde87b6ad41788ac86ee2f8b8f77b94
smool <igorfb4@hotmail.com>
2025-05-06 20:33:07 -0300

    Update AppMain.py

--- AppMain.py
+++ AppMain.py
@@ -1,16 +1,37 @@
 from PY_YOUTUBE_SEARCH_ssmool import GetSearchList
 
-def Contiue():
-   choice = input("Continue [ 0 - YES, 1 - NO ]:")
-   if(choice != '0' or lower(choice) == 'yes' or lower(choice) == 'y'):
+#GLOBAL VARIABLES
+lst = []
+
+def GetChoice():
+    lst_max = str(len(lst))
+    choice = input(f"Choice your video to open[0 - {lst_max} : C - New Search]:")
+    if(choice.lower() == 'c' or choice.lower() == 'continue' or choice == '' or type(int(choice)) != type(0) ):
+        AppStart()
+    else:
+        index = int(choice)
+        index -= 1
+        if(index > 0 or index < int(lst_max)):
+            content = lst[index]
+            api_addr = str(content[3])
+            api_addr = api_addr.replace("{","").replace("'","")
+            webbrowser.open(api_addr)
+            Continue()
+
+
+def Continue():
+   choice = input("New Search [ 0 - YES, 1 - NO, Q - Exit ]:")
+   if(choice == '0' or choice.lower == 'yes' or choice.lower == 'y'):
       AppStart()
-   if(choice == '1' or lower(choice) == 'no' or lower(choice) == 'n'):
+   if(choice == '1' or choice.lower == 'no' or choice.lower == 'n'):
+      GetChoice()
+   if(choice.lower() == 'q' or choice.lower() == 'QUIT' or choice.lower() == 'EXIT'):
       print('By, PY_YOUTUBE_SEARCH quited!')
 
 def AppStart():
+    lst.clear()
     max_results = 20
     query = input("Search by videos(Youtube API):")
     GetSearchList(query, max_results)
-    Continue()
 
 AppStart()
